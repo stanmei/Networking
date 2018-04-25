6b06e537f0d1d9f6e7959b2dad14492a5d1681a2
stanmei <sitao.mei@gmail.com>
2018-04-24 21:39:13 -0700

    admin full options bringup

--- dist_hc_sys/conn/inc/connexch.h
+++ dist_hc_sys/conn/inc/connexch.h
@@ -3,4 +3,10 @@ int ConnTx(int socket,cJSON* txmsg);
 
 cJSON* ConnRx(int socket);
 
+int ConnTxCjsonnt_clt (int client_sock,char* user_grp,char* user_name,char* user_pswd,char* cmd,char* ary_arguments[]);
 
+int ConnRxCjsonnt_clt (int accept_sock,char* ret_msgs);
+
+int ConnRxCjsonnt_srv (int accept_sock,char* user_grp,char* user_name,char* user_pswd,char* cmd,char* ary_arguments[]) ;
+
+int ConnTxCjsonnt_srv (int accept_sock,int ret_val, char* ret_msg, char* ary_ret_msg[],int ary_ret_len) ;
